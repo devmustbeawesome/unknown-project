494772c5979160743f3ca8aa9352396fa59e2e7f
onton <programm.rechner@gmail.com>
2024-01-20 17:49:04 +0400

    add eslint, prettier, stylelint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/eslint-module', '@vueuse/nuxt', '@nuxtjs/stylelint-module', 'nuxt-swiper'],
+  // eslint: {},
+  // stylelint: {},
+  swiper: {
+    // Swiper options
+    //  ----------------------
+    // prefix: 'Swiper',
+    // styleLang: 'css',
+    // modules: ['navigation', 'pagination'], // all modules are imported by default
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        index page
-    </div>
-  </template>
-  
+  <div>index page</div>
+</template>
+
+<script></script>
+<style></style>
