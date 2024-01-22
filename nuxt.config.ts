d20c71e91bc546a71a70481ec7982608aac7613a
onton <programm.rechner@gmail.com>
2024-01-22 09:35:18 +0400

    build: remove prettier file and fix files by lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/eslint-module', '@vueuse/nuxt', '@nuxtjs/stylelint-module', 'nuxt-swiper'],
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@vueuse/nuxt",
+    "@nuxtjs/stylelint-module",
+    "nuxt-swiper",
+  ],
   // eslint: {},
   // stylelint: {},
   swiper: {
