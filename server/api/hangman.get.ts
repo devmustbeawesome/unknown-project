7653ab9d62a508362fb6ab347af967e23bd9297c
devmustbeawesome <programm.rechner@gmail.com>
2024-03-03 03:08:06 +0400

    feat(words): only 5 letter words

--- server/api/hangman.get.ts
+++ server/api/hangman.get.ts
@@ -1,19 +1,9 @@
-import { words } from "~/public/words.json";
+import words from "~/public/words.json";
 function getRandomInt(min: number, max: number) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 export default defineEventHandler(() => {
-  // const fPath = path.join(process.cwd(), "public", "words.json");
-  // let dataWords: { words: [] } = { words: [] };
-  // try {
-  //   const data = fs.readFileSync(fPath, "utf8");
-  //   dataWords = JSON.parse(data);
-  // } catch (err: any) {
-  //   console.log(err);
-  // }
-
-  const word = words[getRandomInt(0, words.length - 1)];
-
+  const word = words.words[getRandomInt(0, words.words.length - 1)];
   return word;
 });
