6decdd96b0f3fbc131fa5e15b9ded26c5b1c6229
devmustbeawesome <programm.rechner@gmail.com>
2024-03-03 02:44:40 +0400

    fix(json path): fix getting json

--- server/api/hangman.get.ts
+++ server/api/hangman.get.ts
@@ -1,20 +1,19 @@
-import fs from "fs";
+import { words } from "~/public/words.json";
 function getRandomInt(min: number, max: number) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 export default defineEventHandler(() => {
-  const fPath = `./assets/words.json`;
-  let dataWords: { words: [] } = { words: [] };
-  try {
-    const data = fs.readFileSync(fPath, "utf8");
-    dataWords = JSON.parse(data);
-  } catch (err: any) {
-    console.log(err);
-  }
+  // const fPath = path.join(process.cwd(), "public", "words.json");
+  // let dataWords: { words: [] } = { words: [] };
+  // try {
+  //   const data = fs.readFileSync(fPath, "utf8");
+  //   dataWords = JSON.parse(data);
+  // } catch (err: any) {
+  //   console.log(err);
+  // }
 
-  const word: { word: string } =
-    dataWords.words[getRandomInt(0, dataWords.words.length - 1)];
+  const word = words[getRandomInt(0, words.length - 1)];
 
   return word;
 });
