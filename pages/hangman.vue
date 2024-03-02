dd178ac1d708efa35a60f4a5e847def3bcf2bde6
devmustbeawesome <programm.rechner@gmail.com>
2024-03-03 00:33:12 +0400

    feat(hangman): create draft version of the game

---
+++ pages/hangman.vue
@@ -0,0 +1,152 @@
+<script lang="ts" setup>
+import type { FetchError } from "ofetch";
+
+const triesLeft = ref(6);
+const gameEnded = ref(false);
+const usedLetters: Ref<Array<string>> = ref([]);
+const {
+  data: word,
+  // pending: pendingGetWord,
+  // error: errorGetUser,
+  execute: getWord,
+} = await useAsyncData<Array<string>, FetchError>(
+  "hangmanGetWord",
+  async () => {
+    return await (
+      await $fetch<String>(`/api/hangman`, {
+        method: "get",
+      })
+    ).split("");
+  },
+  {
+    lazy: true,
+  },
+);
+watch(
+  usedLetters,
+  () => {
+    if (compareArrays(word.value ?? [], usedLetters.value)) {
+      gameEnded.value = true;
+    }
+  },
+  { deep: true },
+);
+watch(
+  triesLeft,
+  () => {
+    if (triesLeft.value <= 0) {
+      triesLeft.value = 0;
+      gameEnded.value = true;
+    }
+  },
+  { deep: true },
+);
+const keyEventHandler = (e: KeyboardEvent) => {
+  const charCode = e.key.charCodeAt(0);
+  if (charCode >= 1040 && charCode <= 1103) {
+    const eKey = e.key.toLowerCase();
+    if (!usedLetters.value.includes(eKey)) {
+      usedLetters.value.push(eKey);
+    }
+    if (!word.value?.includes(eKey)) {
+      triesLeft.value--;
+    }
+  }
+};
+const compareArrays = (a: Array<string>, b: Array<string>) => {
+  for (let i = 0, length = a.length; i < length; i++) {
+    if (!b.includes(a[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+const refreshGame = () => {
+  gameEnded.value = false;
+  triesLeft.value = 6;
+  usedLetters.value = [];
+  getWord();
+};
+onMounted(() => {
+  document.addEventListener("keypress", keyEventHandler);
+});
+onUnmounted(() => {
+  document.removeEventListener("keypress", keyEventHandler);
+});
+</script>
+
+<template>
+  <div class="hangman-game-container">
+    <div class="game_header">
+      <h1>Hangman</h1>
+      <p>Tries left {{ triesLeft }}</p>
+    </div>
+    <div class="game_body">
+      <div class="game_body-left">
+        <HangmanSvg :tries-left="triesLeft" />
+      </div>
+      <div class="game_body-right">
+        <div class="used_letters">
+          Использованные буквы:<br />
+          {{ usedLetters.join(" ") }}
+        </div>
+        <div class="word_wrapper">
+          <div v-for="(letter, index) in word" :key="index" class="word_letter">
+            <span v-if="usedLetters.includes(letter)">{{ letter }}</span>
+            <span v-else> &nbsp;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button v-if="gameEnded" @click="refreshGame">Refresh</button>
+    <p v-if="gameEnded">Game ended</p>
+  </div>
+</template>
+
+<style scoped>
+.hangman-game-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: nowrap;
+}
+
+.game_header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: nowrap;
+}
+
+.game_body {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+}
+
+.game_body-right {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.word_wrapper {
+  display: flex;
+}
+
+.word_letter {
+  border-bottom: 2px solid var(--accent-color);
+  padding-left: 5px;
+  padding-right: 5px;
+  margin: 5px;
+}
+</style>
---
+++ server/api/hangman.get.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+function getRandomInt(min: number, max: number) {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+export default defineEventHandler(() => {
+  const fPath = `./server/api/words.json`;
+  let dataWords: { words: [] } = { words: [] };
+  try {
+    const data = fs.readFileSync(fPath, "utf8");
+    dataWords = JSON.parse(data);
+  } catch (err: any) {
+    console.log(err);
+  }
+
+  const word: { word: string } =
+    dataWords.words[getRandomInt(0, dataWords.words.length - 1)];
+
+  return word;
+});
